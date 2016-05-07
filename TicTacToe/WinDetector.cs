9a223950d49c3ef692c7652331b3de864635bf10
Craig Bass <craig@devls.co.uk>
2016-05-07 12:10:43 +0100

    Initial test harness around win detection, basic types for win detection

---
+++ TicTacToe/WinDetector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TicTacToe.WinDetector.BoardCellState;
+
+namespace TicTacToe
+{
+    public class WinDetector
+    {
+        public WinState Execute( BoardCellState[] board )
+        {
+            if (board[0].Equals(X))
+            {
+                return WinState.CrossesWon;
+            }
+            return WinState.Undecided;
+        }
+
+        public enum BoardCellState
+        {
+            _,X,O
+        }
+
+        public enum WinState
+        {
+            Undecided,
+            CrossesWon,
+            NoughtsWon,
+            Draw
+        }
+    }
+}
---
+++ TicTacToeTest/WinDetectorTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TicTacToe;
+using Should;
+using static TicTacToe.WinDetector;
+using static TicTacToe.WinDetector.BoardCellState;
+using static TicTacToe.WinDetector.WinState;
+
+namespace TicTacToeTest
+{
+
+    [TestFixture]
+    public class WinDetectorTest
+    {
+
+        private WinState WinStateWithEmptyBoard()
+        {
+            return WinStateWithBoard(new[] {
+                _, _, _,
+                _, _, _,
+                _, _, _
+            });
+        }
+        
+        private WinState WinStateWithBoard(BoardCellState[] board)
+        {
+            return winDetector.Execute(board);
+        }
+
+        private WinDetector winDetector;
+        
+        [SetUp]
+        public void Init()
+        {
+            winDetector = new WinDetector();
+        }
+
+        [Test]
+        public void GivenEmptyBoard_ThenWinStateShouldBeUndecided()
+        {
+            WinStateWithEmptyBoard().ShouldEqual( Undecided );
+        }
+        
+        [Test]
+        public void GivenThreeCrossesInSequenceOnTopRow_ThenWinStateShouldBeCrossesWon()
+        {
+            WinStateWithBoard(new[] {
+                X, X, X,
+                _, _, _,
+                _, _, _
+            }).ShouldEqual( CrossesWon );
+        }
+
+    }
+}
